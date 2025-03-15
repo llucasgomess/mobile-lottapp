e50018079d865eb97c5f1a5d5a104c3ec5016427
Lucas Gomes <llucas.gomes.dev@gmail.com>
2025-03-15 16:59:29 -0400

    :ok_hand: style: aplicado o 'prettier'

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,17 @@
-import '@/styles/global.css';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import '@/styles/global.css'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { StatusBar } from 'expo-status-bar'
+import 'react-native-reanimated'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from '@/hooks/useColorScheme'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
   // const [loaded] = useFonts({
   //   SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   // });
@@ -28,13 +28,14 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{
-        headerShown:false
-      }}>
+      <Stack
+        screenOptions={{
+          headerShown: false
+        }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         {/* <Stack.Screen name="+not-found" /> */}
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
-  );
+  )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native'
 
-export default function Index(){
-  return(
+export default function Index() {
+  return (
     <View className="flex-1 bg-red-500">
       <Text> Ola mundo</Text>
     </View>
